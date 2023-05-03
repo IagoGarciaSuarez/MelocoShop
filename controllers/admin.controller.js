bf33788d7bf9fbe8dcb2e41b06b011f40320df1f
Iago García Suárez <iago.garcia.suarez.ds@gmail.com>
2023-05-03 16:57:53 +0200

    crud ops in products

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,15 +1,77 @@
-function getProducts(req, res) {
-  res.render("admin/products/all-products");
+const Product = require("../models/product.model");
+
+async function getProducts(req, res, next) {
+  try {
+    const products = await Product.findAll();
+    res.render("admin/products/all-products", { products: products });
+  } catch (error) {
+    return next(error);
+  }
 }
 
-function getNewProduct(req, res) {
+function getNewProduct(req, res, next) {
   res.render("admin/products/new-product");
 }
 
-function createNewProduct() {}
+async function createNewProduct(req, res) {
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
+
+  try {
+    await product.save();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.redirect("/admin/products");
+}
+
+async function getUpdateProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("admin/products/update-product", { product: product });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id,
+  });
+
+  if (req.file) {
+    product.replaceImage(req.file.filename);
+  }
+
+  try {
+    await product.save();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.redirect("/admin/products");
+}
+
+async function deleteProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    await product.remove();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.json({ message: "Deleted product!" });
+}
 
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
   createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct,
+  deleteProduct: deleteProduct,
 };
